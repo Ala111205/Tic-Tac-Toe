5baa9bd810022bbec3bb7d0fba16b89a98bf69e6
Ala111205 <sadham070403@gmail.com>
2025-09-03 14:32:18 +0530

    initial

--- script.js
+++ script.js
@@ -1,175 +1,117 @@
-const gridEl = document.getElementById("grid");
-const newGameBtn = document.getElementById("newGameBtn");
-const movesEl = document.getElementById("moves");
-const timeEl = document.getElementById("time");
-const scoreEl = document.getElementById("score");
-const difficultyEl = document.getElementById("difficulty");
-const modal = document.getElementById("modal");
-const modalMsg = document.getElementById("modalMsg");
-const playAgainBtn = document.getElementById("playAgainBtn")
+const board = document.getElementById("board");
+const statusText = document.getElementById("status");
+const cells = document.querySelectorAll(".cell");
+const resetBtn = document.getElementById("resetBtn");
 
-const EMOJI_SET = ["🍎","🍌","🍇","🍉","🍓","🍒","🍍","🥝","🥑","🍑","🍋","🍊","🍐","🥥","🥕","🌽","🍆","🍅"];
+const scoreX = document.getElementById("scoreX");
+const scoreO = document.getElementById("scoreO");
+const scoreTie = document.getElementById("scoreTie");
 
-let gridSize = 4;
-let deck = [];
-let lockBoard = false;
-let firstPick = null;
-let secondPick = null;
-let moves = 0;
-let matchPairs = 0;
-let startTime = null;
-let timeInterval = null;
+const scores = {X:0, O:0, Tie:0};
+let lastWinner
 
-function shuffle(array){
-  for(let i = array.length-1; i>0; i--){
-    const j = Math.floor(Math.random()*(i+1));
-    [array[i], array[j]] = [array[j], array[i]]
-  };
-  return array;
-}
+let gameActive = true;
+let currentPlayer = "X"
+let gameState = ["","","","","","","","",""]
+const winningConditions = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+];
 
-function formatTime(ms){
-  const sec = Math.floor(ms/1000);
-  const m = String(Math.floor(sec/60)).padStart(2,"0");
-  const s = String(Math.floor(sec%60)).padStart(2,"0");
+function handleCellClick(e){
+    const cell = e.target;
+    const index = cell.getAttribute("data-index");
 
-  return `${m}:${s}`;
-}
+    if(!gameActive || gameState[index] !== "") return;
 
-function buildeck(size){
-  const pairs = (size*size)/2;
-  const symbol = EMOJI_SET.slice(0, pairs);
-  const cards = symbol.flatMap((s,i)=>[
-    {id: `${s}-${i}-A`, symbol: s},
-    {id: `${s}-${i}-B`, symbol: s}
-  ])
-  return shuffle(cards)
-}
+    gameState[index] = currentPlayer;
+    cell.textContent = currentPlayer;
 
-function renderCards(){
-  gridEl.style.gridTemplateColumns = `repeat(${gridSize}, 0fr)`
-  gridEl.innerHTML="";
-
-  const frag = document.createDocumentFragment();
-  deck.forEach((card)=>{
-    const cardEl = document.createElement("div");
-    cardEl.className = "card";
-    cardEl.dataset.symbol = card.symbol
-
-    const inner = document.createElement("div");
-    inner.className = "card-inner";
-
-    const front = document.createElement("div");
-    front.className = "face front";
-
-    const back = document.createElement("div");
-    back.className = "face back";
-    back.textContent = card.symbol;
-
-    inner.appendChild(front);
-    inner.appendChild(back);
-    cardEl.appendChild(inner);
-
-    frag.appendChild(cardEl);
-
-    cardEl.addEventListener("click", ()=>onCardClick(cardEl));
-  });
-
-  gridEl.appendChild(frag)
+    checkResult();
 };
 
-function onCardClick(cardEl){
-  if(lockBoard) return;
+function checkResult(){
+    let roundWon = false;
+    let winningCombo = [];
 
-  if(cardEl.classList.contains("is-flipped")) return;
-
-  if(!timeInterval){
-    startTime = Date.now();
-    timeInterval = setInterval(()=>{
-      timeEl.textContent = formatTime(Date.now()-startTime);
-      updateScore();
-    },500)
-  }
-
-  cardEl.classList.add("is-flipped");
-
-  if(!firstPick){
-    firstPick = cardEl;
-    return;
-  }
-
-  secondPick = cardEl;
-  moves++;
-  movesEl.textContent=moves;
-
-  updateScore();
-  
-  checkMatch();
-}
-
-function checkMatch(){
-  if(firstPick.dataset.symbol === secondPick.dataset.symbol){
-    matchPairs++
-    resetPicks();
-    if(matchPairs === deck.length/2){
-      onWin();
+    for(let condition of winningConditions){
+        const [a,b,c] = condition;
+        if(gameState[a] && gameState[b] === gameState[a] && gameState[a] === gameState[c]){
+            roundWon = true;
+            winningCombo = [a,b,c];
+            break;
+        }
     }
-  }
-  else{
-    lockBoard = true;
-    setTimeout(()=>{
-      firstPick.classList.remove("is-flipped")
-      secondPick.classList.remove("is-flipped")
 
-      resetPicks();
-    },800)
-  }
-}
+    if(roundWon){
+        statusText.textContent = `Player ${currentPlayer} Win! 🎉`;
+        highlightWinner(winningCombo);
+        updateScore(currentPlayer);
 
-function resetPicks(){
-  [firstPick, secondPick] = [null, null];
-  lockBoard = false;
-}
+        lastWinner = currentPlayer;
+        board.classList.add("disabled")
+        gameActive = false;
+        return;
+    }
 
-function newGame(){
-  clearInterval(timeInterval);
-  timeInterval=null;
-  startTime=null;
-  timeEl.textContent="00:00"
-  moves=0;
-  matchPairs=0;
-  movesEl.textContent=0;
-  scoreEl.textContent=0;
-  deck=buildeck(gridSize);
-  renderCards();
-  modal.hidden = true;
-}
+    if(!gameState.includes("")){
+        statusText.textContent = `It's a Tie 🤝`;
+        updateScore("Tie");
 
-function updateScore(){
-  const elapsed = startTime? (Date.now()-startTime)/1000: 0;
-  const score= Math.max(0,10000-Math.floor(elapsed*15)-moves*120)
+        gameActive = false;
+        return;
+    }
 
-  scoreEl.textContent=score;
-}
+    currentPlayer = currentPlayer === "X"? "O": "X";
+    statusText.textContent = `Player ${currentPlayer}'s Turn`;
+};
 
-function onWin(){
-  clearInterval(timeInterval);
-  const elapsed = Date.now()-startTime;
-  modalMsg.textContent = `Finshed in ${formatTime(elapsed)} with ${moves} moves. Score: ${scoreEl.textContent}`;
-  modal.hidden = false;
-}
+function highlightWinner(combo){
+    combo.forEach(index=>{
+        cells[index].classList.add("winner")
+    });
+};
 
-newGameBtn.addEventListener("click", ()=>{
-  newGame();
-})
+function updateScore(winner){
+    if(winner === "X"){
+        scores.X++;
+        scoreX.textContent = `PlayerX: ${scores.X}`;
+    }
+    else if(winner === "O"){
+        scores.O++;
+        scoreO.textContent = `PlayerO: ${scores.O}`;
+    }
+    else{
+        scores.Tie++;
+        scoreTie.textContent = `Tie: ${scores.Tie}`;
+    }
+};
 
-playAgainBtn.addEventListener("click", ()=>{
-  newGame();
-})
+function reset(){
+    gameState = ["","","","","","","","",""];
 
-difficultyEl.addEventListener("click", (e)=>{
-  gridSize=parseInt(e.target.value);
-  newGame();
-});
+    board.classList.remove("disabled");
+    cells.forEach(cell=>{
+        cell.innerHTML="";
+        cell.classList.remove("winner");
+    });
 
-newGame();
+   
+    if(!gameActive){
+        currentPlayer = lastWinner;
+        statusText.textContent = `Start with Winner${lastWinner}`;
+    }
+    else if(gameActive && scoreTie){
+        statusText.textContent = `Start with Player${currentPlayer}`;
+        if(lastWinner){
+            currentPlayer = lastWinner;
+            statusText.textContent = `Winner ${currentPlayer}'s Turn`;
+        }
+    }
+
+    gameActive = true;
+};
+
+cells.forEach(cell=>cell.addEventListener("click",handleCellClick));
+resetBtn.addEventListener("click",reset);
